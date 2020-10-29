d38f2e374a557cc5081ca2b25dd5535ce2b9c442
ycodex <karthiknaik120@gmail.com>
2020-10-29 22:06:19 +0530

    loader not working, table scroll bug, and some small bugs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,140 @@
+var searchResultFormat = '<tr><td><a href="#" target="_blank">$title</a></td><td align="left">$desp</td></tr>';
+var totalLimit = 100;
 
-function mainpage(){
-    var b=document.getElementById('body');
-    
-    var elem = document.getElementById("animate");
-    elem.style.animationDuration="3s";
-    elem.style.position='relative';
-    elem.style.animation='none';
-    var sbox=document.getElementById("searchbox");
-    
-  
-}
+var controls = {
+    oldColor: '',
+    displayResults: function() {
+        if (results.style) {
+            results.style.display = '';
+        }
+        resultsTableHideable.classList.remove('hide');
+    },
+    hideResults: function() {
+        if (results.style) {
+            results.style.display = 'none';
+        }
+        resultsTableHideable.classList.add('hide');
+    },
+    doSearch: function(match, dataset) {
+        results = [];
+
+        words = match.toLowerCase();
+        words = words.split(' ');
+        regex = '';
+        // Lazy way to create regex (?=.*word1)(?=.*word2) this matches all words.
+        for (i = 0; i < words.length; i++) {
+            regex += '(?=.*' + words[i] + ')';
+        }
+
+        dataset.forEach(e => {
+            
+
+        if (e.title.toLowerCase().match(regex)) results.push(e);
+        });
+        return results;
+    },
+    updateResults: function(loc, results) {
+        if (results.length == 0) {
+            noResults.style.display = '';
+            noResults.textContent = 'No Results Found';
+
+            resultsTableHideable.classList.add('hide');
+        } else if (results.length > totalLimit) {
+            noResults.style.display = '';
+            resultsTableHideable.classList.add('hide');
+            noResults.textContent = 'Error: ' + results.length + ' results were found, try being more specific';
+            this.setColor(colorUpdate, 'too-many-results');
+        } else {
+            var tableRows = loc.getElementsByTagName('tr');
+            for (var x = tableRows.length - 1; x >= 0; x--) {
+                loc.removeChild(tableRows[x]);
+            }
+
+            noResults.style.display = 'none';
+            resultsTableHideable.classList.remove('hide');
+
+            results.forEach(r => {
+                //Not the fastest but it makes for easier to read code :>
+
+               
+                el = searchResultFormat
+                    .replace('$title', r.title)
+                    .replace('$desp', r.shortDescription);
+                    //.replace('$status', linkTemplate.replace('$video', r.videoId).replace('$time', timeInSeconds));
+
+                var wrapper = document.createElement('table');
+                wrapper.innerHTML = el;
+                var div = wrapper.querySelector('tr');
+
+                loc.appendChild(div);
+            });
+        }
+    },
+    setColor: function(loc, indicator) {
+        if (this.oldColor == indicator) return;
+        var colorTestRegex = /^color-/i;
+
+        loc.classList.forEach(cls => {
+            //we cant use class so we use cls instead :>
+            if (cls.match(colorTestRegex)) loc.classList.remove(cls);
+        });
+        loc.classList.add('color-' + indicator);
+        this.oldColor = indicator;
+    }
+};
+window.controls = controls;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    results = document.querySelector('div.results');
+    searchValue = document.querySelector('input.search');
+    form = document.querySelector('form.searchForm');
+    resultsTableHideable = document.getElementsByClassName('results-table').item(0);
+    resultsTable = document.querySelector('tbody.results');
+    resultsTable = document.querySelector('tbody.results');
+    noResults = document.querySelector('div.noResults');
+    colorUpdate = document.body;
+
+    document.body.classList.add('fade');
+
+    var currentSet = [];
+    var oldSearchValue = '';
+
+    function doSearch(event) {
+        var val = searchValue.value;
+
+        if (val != '') {
+            controls.displayResults();
+            currentSet = window.dataset;
+            oldSearchValue = val;
+
+            currentSet = window.controls.doSearch(val, currentSet);
+            if (currentSet.length < totalLimit) window.controls.setColor(colorUpdate, currentSet.length == 0 ? 'no-results' : 'results-found');
+
+            window.controls.updateResults(resultsTable, currentSet);
+        } else {
+            controls.hideResults();
+            window.controls.setColor(colorUpdate, 'no-search');
+            noResults.style.display = 'none';
+            currentSet = window.dataset;
+        }
+
+        if (event.type == 'submit') event.preventDefault();
+    }
+
+
+    fetch('../Database/books.json')
+        .then(res => res.json())
+        .then(data => {
+            window.dataset = data;
+            currentSet = window.dataset;
+            window.controls.updateResults(resultsTable, window.dataset);
+            doSearch({ type: 'none' });
+        });
+
+    form.submit(doSearch);
+
+    searchValue.addEventListener('input', doSearch);
+
+
+})
