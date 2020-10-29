bf2b4dd1f5858cdcbbf5e40a093471280c41ca83
ycodex <karthiknaik120@gmail.com>
2020-10-29 15:45:04 +0530

    Initial page with layouts

---
+++ ReadME.md
@@ -0,0 +1,3 @@
+INITIAL IDEAS:
+1.Email validator (mailbox validator api)
+2.Book finder (google books api) 3.
---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function mainpage(){
+    var b=document.getElementById('body');
+    
+    var elem = document.getElementById("animate");
+    elem.style.animationDuration="3s";
+    elem.style.position='relative';
+    elem.style.animation='none';
+    var sbox=document.getElementById("searchbox");
+    
+  
+}
